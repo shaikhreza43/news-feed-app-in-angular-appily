8204510150072ccc0d3ffe4d677bfa65b27f8afc
shaikhreza43 <reza.rk786@gmail.com>
2020-08-06 19:55:02 +0530

    Some Bug fixes

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,20 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(public http: HttpClient) {}
 
-  constructor(public http:HttpClient) { }
-
-  public fetchUrl='https://banking-appi-ly.herokuapp.com/posts';
+  public fetchUrl = 'https://banking-appi-ly.herokuapp.com/posts';
   public apiData;
+  public card1Arr = [];
+  public innerCardArr = [];
 
-  fetchDataFromApi=()=>{
-    this.http.get(this.fetchUrl).subscribe(data=>{
-      this.apiData=data;
+  fetchDataFromApi = () => {
+    this.http.get(this.fetchUrl).subscribe((data) => {
+      this.apiData = data;
+      // this.card1Arr= data['replies'];
+      // if(data['replies'][0]['replies']){
+      //   this.innerCardArr = data['replies'][0]['replies'];
+      // }
+      // console.log(data['replies'][0]['replies']);
+
+      // console.log(this.card1Arr);
     });
-  }
-
-
+  };
 }
--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -5,14 +5,12 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.css']
+  styleUrls: ['./feed.component.css'],
 })
 export class FeedComponent implements OnInit {
-
-  constructor(public api:ApiService,public http:HttpClient) { }
+  constructor(public api: ApiService, public http: HttpClient) {}
 
   ngOnInit(): void {
     this.api.fetchDataFromApi();
   }
-
 }
